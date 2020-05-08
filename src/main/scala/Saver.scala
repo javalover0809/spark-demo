05c79fd3254db094c0170679a32901d9112f3583
唐国洁 <tangguojie@meituan.com>
2020-05-08 20:06:40 +0800

    mart_catering.dwa_crm_tutorial_test

--- src/main/scala/Saver.scala
+++ src/main/scala/Saver.scala
@@ -2,7 +2,7 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 class Saver(spark: SparkSession, df: DataFrame, table: String) extends TableSaver(spark, df, table) {
   def getCreateSQL(table: String): String =
     """
-       |create table if not exists $table
+       |create table if not exists mart_catering.dwa_crm_tutorial_test
        |(
        |    `poi_id` bigint comment'门店ID',
        |    `spu_cnt` string comment"菜品计数"
@@ -13,7 +13,7 @@ class Saver(spark: SparkSession, df: DataFrame, table: String) extends TableSave
 
   override def getWriteSQL(table: String, tempView: String): String =
     s"""
-       |insert overwrite table $table
+       |insert overwrite table mart_catering.dwa_crm_tutorial_test
        |select
        |       poi_id,
        |       spu_cnt
